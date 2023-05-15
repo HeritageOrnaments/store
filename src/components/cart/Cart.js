4d98be42a7552dc184435dd74c8898e20c7c542d
dharmeshgurnani <dharmeshgurnani2000@gmail.com>
2023-05-15 23:41:30 +0530

    feat(cart page iteam hendeler)

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,84 +1,53 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  HStack,
-  Link,
-  Stack,
-  useColorModeValue as mode,
-} from '@chakra-ui/react'
+import { Box, Flex, Heading, HStack, Link, Stack, useColorModeValue as mode, } from '@chakra-ui/react'
+import { useSelector } from 'react-redux'
 import { CartItem } from './components/CartItem.js';
 import { CartOrderSummary } from './components/CartOrderSummary';
 import Header from '../common/Header.js';
 import Footer from '../common/Footer.js';
-const cartData = [
-  {
-    id: '1',
-    price: 550,
-    currency: 'INR',
-    name: 'White necklace',
-    description: 'White necklace',
-    quantity: 1,
-    imageUrl: 'https://media.graphassets.com/tv8bAGfaTT2KdPaAxu27',
-  }
-];
-export const Cart = () => (
-  <><Header /><Box
-    maxW={{
-      base: '3xl',
-      lg: '7xl',
-    }}
-    mx="auto"
-    px={{
-      base: '4',
-      md: '8',
-      lg: '12',
-    }}
-    py={{
-      base: '6',
-      md: '8',
-      lg: '12',
-    }}
-  >
-    <Stack
-      direction={{
-        base: 'column',
-        lg: 'row',
-      }}
-      align={{
-        lg: 'flex-start',
-      }}
-      spacing={{
-        base: '8',
-        md: '16',
-      }}
-    >
-      <Stack
-        spacing={{
-          base: '8',
-          md: '10',
-        }}
-        flex="2"
-      >
-        <Heading fontSize="2xl" fontWeight="extrabold">
-          Shopping Cart (3 items)
-        </Heading>
+import { useDispatch } from 'react-redux';
+import { incrementQuantity, decrementQuantity, removeItem} from '../../redux/cartSlice.js';
+function Cart() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  const getTotalQuantity = () => {
+    let total = 0
+    cart.forEach(item => {
+      total += item.quantity
+    })
+    return total
+  };
+  return (
+    <>
+      <Header />
+      <Box
+        maxW={{ base: '3xl', lg: '7xl', }} mx="auto" px={{ base: '4', md: '8', lg: '12', }}
+        py={{ base: '6', md: '8', lg: '12', }} >
+        <Stack direction={{ base: 'column', lg: 'row', }} align={{ lg: 'flex-start', }} spacing={{ base: '8', md: '16', }} >
+          <Stack spacing={{ base: '8', md: '10', }} flex="2" >
+            <Heading fontSize="2xl" fontWeight="extrabold"> Shopping Cart ({getTotalQuantity() || 0} items) </Heading>
+            <Stack spacing="6">
+              {cart?.map((item) => (
+                <CartItem
+                  key={item.id} id={item.id} imageUrl={item.image}
+                  title={item.title} price={item.price} quantity={item.quantity}
+                  onClickDelete={()=>{dispatch(removeItem(item.id))}}
+                  incrementQuantity={()=>{dispatch(incrementQuantity(item.id))}}
+                  decrementQuantity={()=>{dispatch(decrementQuantity(item.id))}}
+                />
+              ))}
+            </Stack>
+          </Stack>
 
-        <Stack spacing="6">
-          {cartData.map((item) => (
-            <CartItem key={item.id} {...item} />
-          ))}
+          <Flex direction="column" align="center" flex="1">
+            <CartOrderSummary />
+            <HStack mt="6" fontWeight="semibold">
+              <p>or</p> <Link color={mode('blue.500', 'blue.200')}>Continue shopping</Link>
+            </HStack>
+          </Flex>
         </Stack>
-      </Stack>
-
-      <Flex direction="column" align="center" flex="1">
-        <CartOrderSummary />
-        <HStack mt="6" fontWeight="semibold">
-          <p>or</p>
-          <Link color={mode('blue.500', 'blue.200')}>Continue shopping</Link>
-        </HStack>
-      </Flex>
-    </Stack>
-  </Box><Footer /></>
-)
+      </Box>
+      <Footer />
+    </>
+  )
+}
 export default Cart;
--- src/components/cart/components/CartItem.js
+++ src/components/cart/components/CartItem.js
@@ -1,89 +1,39 @@
-import { CloseButton, Flex, Link, Select, useColorModeValue } from '@chakra-ui/react'
+import { CloseButton, Flex, Link,Input, HStack, Button } from '@chakra-ui/react'
 import { PriceTag } from './PriceTag.js'
 import { CartProductMeta } from './CartProductMeta.js'
-const QuantitySelect = (props) => {
+const QuantitySelect = ({value, incrementQuantity, decrementQuantity}) => {
+  console.log(value)
   return (
-    <Select
-      maxW="64px"
-      aria-label="Select quantity"
-      focusBorderColor={useColorModeValue('blue.500', 'blue.200')}
-      {...props}
-    >
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-    </Select>
+    <HStack maxW='150px'>
+      <Button onClick={decrementQuantity}>-</Button>
+      <Input textAlign={'center'} readOnly={true} value={value}/>
+      <Button onClick={incrementQuantity}>+</Button>
+    </HStack>
   )
 }
 
 export const CartItem = (props) => {
+  console.table(props)
   const {
-    isGiftWrapping,
-    name,
-    description,
-    quantity,
-    imageUrl,
-    currency,
-    price,
-    onChangeQuantity,
-    onClickDelete,
+    isGiftWrapping, name, description,
+    quantity, imageUrl, currency, price,
+    incrementQuantity, decrementQuantity, onClickDelete,
   } = props
   return (
-    <Flex
-      direction={{
-        base: 'column',
-        md: 'row',
-      }}
-      justify="space-between"
-      align="center"
-    >
-      <CartProductMeta
-        name={name}
-        description={description}
-        image={imageUrl}
-        isGiftWrapping={isGiftWrapping}
-      />
+    <Flex direction={{ base: 'column', md: 'row', }} justify="space-between" align="center" >
+      <CartProductMeta name={name} description={description} image={imageUrl} isGiftWrapping={isGiftWrapping} />
 
       {/* Desktop */}
-      <Flex
-        width="full"
-        justify="space-between"
-        display={{
-          base: 'none',
-          md: 'flex',
-        }}
-      >
-        <QuantitySelect
-          value={quantity}
-          onChange={(e) => {
-            onChangeQuantity?.(+e.currentTarget.value)
-          }}
-        />
+      <Flex width="full" justify="space-between" display={{ base: 'none', md: 'flex', }} >
+        <QuantitySelect value={quantity} incrementQuantity = {incrementQuantity} decrementQuantity = {decrementQuantity} />
         <PriceTag price={price} currency={currency} />
         <CloseButton aria-label={`Delete ${name} from cart`} onClick={onClickDelete} />
       </Flex>
 
       {/* Mobile */}
-      <Flex
-        mt="4"
-        align="center"
-        width="full"
-        justify="space-between"
-        display={{
-          base: 'flex',
-          md: 'none',
-        }}
-      >
-        <Link fontSize="sm" textDecor="underline">
-          Delete
-        </Link>
-        <QuantitySelect
-          value={quantity}
-          onChange={(e) => {
-            onChangeQuantity?.(+e.currentTarget.value)
-          }}
-        />
+      <Flex mt="4" align="center" width="full" justify="space-between" display={{ base: 'flex', md: 'none', }} >
+        <Link fontSize="sm" textDecor="underline"> Delete </Link>
+        <QuantitySelect value={quantity} incrementQuantity = {incrementQuantity} decrementQuantity = {decrementQuantity} />
         <PriceTag price={price} currency={currency} />
       </Flex>
     </Flex>
